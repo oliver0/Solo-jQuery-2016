0185f702e9cfdac327b13900c1e6e9ce36937938
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-07 15:44:27 -0600

    Refactor code to be clear , add comments

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 $('document').ready(function(){
 
-  $('button').on('click', function(){
-    var buttonColor = $(this).attr('data-color');
-    var buttonId = "#"+$(this).attr('data-color');
-    var num = parseInt($(buttonId).find('.counter').text());
-    $(buttonId).find('.counter').text(num+1);
+  $('.color-button').on('click', function(){
 
-    //var squareClass = "."+buttonColor;
-    //console.log(squareClass);
+    // get the color the button is related to
+    var buttonColor = $(this).attr('data-color');
+    // create the button ID with concatenation so it can be used later
+    var buttonId = "#"+$(this).attr('data-color');
+    // create variable of the counter to be incremented
+    var counter = $(buttonId).find('.counter');
+    // make the text into a number
+    var num = parseInt(counter.text());
+    // increment the counter then change the text to this number
+    counter.text(num + 1);
+
+    //append divs to DOM with classes which create styles
     $('.container').append('<div class="' + buttonColor +  ' color-cube"</div>');
   });
 });
